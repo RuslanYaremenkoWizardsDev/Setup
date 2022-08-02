ac291e1eed581fa6bd4a17b2032619342ae809b7
RuslanYaremenkoWizardsDev <ruslany@wizardsdev.com>
2022-08-02 16:58:03 +0300

    first build

--- pages/example/Example.tsx
+++ pages/example/Example.tsx
@@ -1,21 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
 import ExampleItem from './ExampleItem';
-import { helper } from './helper';
 
 interface IProps {
 
 }
 
-const Example: React.FC<IProps> = () => {
-  helper();
-  return (
-    <div>
-      <h1>example</h1>
-      <ExampleItem />
-      <Link href="/">Home</Link>
-    </div>
-  );
-};
+const Example: React.FC<IProps> = () => (
+  <div>
+    <h1>exampleasdasdasdsdasd</h1>
+    <ExampleItem />
+    <Link href="/">Home</Link>
+  </div>
+);
 
 export default Example;
